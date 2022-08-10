0f5a7938bddf4ac8ec66c1cebd12a5fbf07f300c
Alireza Shakouri <alirezasha01gmail.com>
2022-08-10 14:45:25 +0430

    created input and button and css style for input and button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
+import "./App.css";
 class App extends React.Component{
     render(){
         return(
-            <div>
-                <p>test</p>
+            <div className="center">
+                <h1>Todos List</h1>
+                <input type="text" className="input" placeholder="Todos"></input>
+                <button type="submit" className="button">Save</button>
             </div>
         )
     }
