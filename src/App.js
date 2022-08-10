728afecd4a5db2a02bbe1c4ae5c60d7c01a4b731
Alireza Shakouri <alirezasha01gmail.com>
2022-08-10 15:52:12 +0430

    This project (Todos) is made for practicing React.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./App.css";
+import ItemList from "./components/itemList/ItemList";
 class App extends React.Component{
     render(){
         return(
@@ -8,6 +9,10 @@ class App extends React.Component{
                 <h1>Todos List</h1>
                 <input type="text" className="input" placeholder="Todos"></input>
                 <button type="submit" className="button">Save</button>
+                <ItemList />
+                <ItemList />
+                <ItemList />
+                <ItemList />
             </div>
         )
     }
---
+++ src/components/itemList/ItemList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./ItemList.css"
+
+const ItemList = () =>{
+    return(
+        <div>
+            <hr/>
+        <div className="divtodo">
+            <div></div>
+            <div>
+            <p className="text">hello</p>
+            </div>
+            <div>
+            <button className="buttonDelete">Delete</button>
+            </div>
+            <div></div>
+        </div>
+        </div>
+    )
+}
+
+export default ItemList;
